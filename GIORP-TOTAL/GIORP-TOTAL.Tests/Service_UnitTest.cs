26fb08a47205a589a3110a4c5660df7555d20a39
Streebor <stefanvasiljevic@me.com>
2015-11-30 12:56:31 -0500

    Registering Team and Service Done.

---
+++ GIORP-TOTAL/GIORP-TOTAL.Tests/Service_UnitTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GIORP_TOTAL.Tests
+{
+    [TestClass]
+    public class Service_UnitTest
+    {
+        [TestMethod]
+        public void CreateRegisterServiceMessage_Normal1()
+        {
+            var message = Service.CreateRegisterServiceMessage();
+            Assert.IsTrue(message != null);
+        }
+    }
+}
