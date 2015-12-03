7a3e3daa413ceae92beca4affd3ced97c1108f66
Aaron <aschryver-cc@conestogac.on.ca>
2015-12-02 19:03:55 -0500

    Added logging

--- GIORP-TOTAL/GIORP-TOTAL/SocketClient.cs
+++ GIORP-TOTAL/GIORP-TOTAL/SocketClient.cs
@@ -36,6 +36,10 @@ namespace GIORP_TOTAL
                 {
                     sender.Connect(remoteEP);
 
+                    Logger.logMessage("Sending service request to IP " + serverName + ", PORT " + port.ToString() + " :" +
+                                      Environment.NewLine +
+                                      "\t>>" + request);
+
                     // Encode the data string into a byte array.
                     byte[] msg = Encoding.ASCII.GetBytes(request);
 
@@ -46,6 +50,10 @@ namespace GIORP_TOTAL
                     int bytesRec = sender.Receive(bytes);
                     response = Encoding.ASCII.GetString(bytes, 0, bytesRec);
 
+                    Logger.logMessage("Response from Published Service at IP " + serverName + ", PORT " + port.ToString() + " :" +
+                                      Environment.NewLine +
+                                      "\t>>" + response);
+
                     // Release the socket.
                     sender.Shutdown(SocketShutdown.Both);
                     sender.Close();
@@ -54,22 +62,30 @@ namespace GIORP_TOTAL
                 catch (ArgumentNullException ane) 
                 {
                     //Console.WriteLine("ArgumentNullException : {0}",ane.ToString());
+                    Logger.logException(ane);
+
                     throw ane;
                 } 
                 catch (SocketException se) 
                 {
                     //Console.WriteLine("SocketException : {0}",se.ToString());
+                    Logger.logException(se);
+
                     throw se;
                 } 
                 catch (Exception e) 
                 {
                     //Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                    Logger.logException(e);
+
                     throw e;
                 }
             } 
             catch (Exception e) 
             {
                 //Console.WriteLine( e.ToString());
+                Logger.logException(e);
+
                 throw e;
             }
 
